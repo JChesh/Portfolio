790e88e42915e32867c57a798e6603256bea2c9e
JChesh <sfuchesh@hotmail.com>
2019-08-25 19:42:59 -0400

    Needs CSS/Links

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
 
-    // // Check for click events on the navbar burger icon
-    // $(".overview").click(function() {
-  
-    //     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    //     $(".overview").toggleClass("is-active");
-  
-    // });
-    // $(".about").click(function() {
-  
-    //     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    //     $(".about").toggleClass("is-active");
-  
-    // });
-    // $(".work").click(function() {
-  
-    //     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    //     $(".work").toggleClass("is-active");
-  
-    // });
-    // $(".contact").click(function() {
-  
-    //     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    //     $(".contact").toggleClass("is-active");
-  
-    // });
-
     $(".overview").toggleClass("is-active");
         $(".about").toggleClass("is-active");
         $(".work").toggleClass("is-active");
